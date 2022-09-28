1014a8a7c49755370b9fcc636a16e580acc74d29
pier2701 <100949448+pier2701@users.noreply.github.com>
2022-09-28 13:26:25 +0200

    Delete front_groupomania directory

--- front_groupomania/src/components/AppContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-// on déclare une variable qui stockera notre "jwt"
-export const UidContext = createContext();
--- front_groupomania/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './user.reducer';
-
-// on centralise les "reducers"
-export default combineReducers({
-    userReducer,
-})
